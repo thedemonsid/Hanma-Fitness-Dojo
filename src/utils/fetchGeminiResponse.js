f33efcc8703342f215f8f549c3efdf0ad1857e92
thedemonsid <siddheshshrirame@gmail.com>
2024-07-20 16:59:53 +0530

    Fix spell mstake and a dynamic url error ( need to look in future)

--- src/utils/fetchGeminiResponse.js
+++ src/utils/fetchGeminiResponse.js
@@ -8,19 +8,17 @@ async function fetchGeminiResponse(userEmail, type) {
   let config = {
     method: "post",
     maxBodyLength: Infinity,
-    url: "/api/" + type,
+    url: process.env.KINDE_SITE_URL + "/api/" + type, //need to think why the dynamic urls were not working
     headers: {
       "Content-Type": "application/json",
     },
     data: data,
   };
-
   try {
     const response = await axios.request(config);
-   // console.log(JSON.stringify(response.data));
     return response.data;
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 }
 export default fetchGeminiResponse;
